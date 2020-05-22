135444a9e333f55eb452eb0ba576dac74f984ed9
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-05-21 23:31:14 -0500

    coded the server googe api file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var db= require("../models")
-
+var axios = require("axios")
 function routes(app){
-    
+    app.get("/api/googlebooks/:title", function(req, res){
+        axios.get("https://www.googleapis.com/books/v1/volumes?q="+ req.params.title).then(function (results){
+            res.json(result.data)
+        })
+    })
 }
 module.exports = routes
