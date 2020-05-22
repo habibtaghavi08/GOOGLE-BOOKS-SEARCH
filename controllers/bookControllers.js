ba869cfdee235ecaec0d16b27927f43116dcf481
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-05-21 21:29:49 -0500

    updated bookControllers.js file

---
+++ controllers/bookControllers.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Book.find(req.query)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    console.log(req.params)
+    db.Book.findById(req.params.id)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book.create(req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book.findById(req.params.id)
+      .then(dbBook => dbBook.remove())
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  }
+};
