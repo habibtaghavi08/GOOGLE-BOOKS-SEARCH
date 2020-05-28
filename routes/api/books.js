73c46bb39bca0f8be0b5584fba09c0f0cb0a9534
habibtaghavi08 <habibtaghavi08@gmail.com>
2020-05-28 04:21:50 -0500

    added books.js folder and fil

---
+++ routes/api/books.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const bookController = require("../../controllers/bookController");
+
+
+router.route("/")
+  .get(bookController.findAll)
+  .post(bookController.create);
+
+router
+  .route("/:id")
+  .get(bookController.findById)
+  .put(bookController.update)
+  .delete(bookController.remove);
+
+module.exports = router;
